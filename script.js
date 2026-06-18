07fdc96983ccaf21fa90d059696b7fba0e93d293
itzzy04 <itziare.gv@gmail.com>
2026-06-18 19:20:02 -0400

    correción mensajito

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ function obtenerSaludo() {
     const hora= new Date().getHours();
 
     if (hora >=6 && hora < 12) {
-        return "¡Buen día :)!";
+        return "Buenos días";
     } else if (hora >=12 && hora < 20) {
-        return "Buenas tardes :)";
+        return "Buenas tardes";
     } else {
-        return "¡Buenas noches :)!";
+        return "Buenas noches";
     }
 }
 function mostrarBienvenida() {
     const elemento = document.getElementById("bienvenida");
-    elemento.innerHTML = obtenerSaludo() + "<br>Bienvenido/a a Patitas Perdidas";
+    elemento.innerHTML = obtenerSaludo() + "<br>¡Bienvenid@ a Patitas Perdidas!";
 }
 document.addEventListener("DOMContentLoaded", function (){
     mostrarBienvenida();
